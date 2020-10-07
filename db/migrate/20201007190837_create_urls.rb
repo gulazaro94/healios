cb8541bbad6c740c13fd23034a4b505b8bb42cfc
Gustavo <gulazaro94@gmail.com>
2020-10-07 21:45:16 +0100

    changing urls.access_count from string to integer

--- db/migrate/20201007190837_create_urls.rb
+++ db/migrate/20201007190837_create_urls.rb
@@ -3,7 +3,7 @@ class CreateUrls < ActiveRecord::Migration[6.0]
     create_table(:urls) do |t|
       t.string :url
       t.string :token, index: true, unique: true, limit: 10
-      t.string :access_count, default: 0
+      t.bigint :access_count, default: 0
 
       t.timestamps
     end
